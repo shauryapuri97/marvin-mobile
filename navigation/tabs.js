a8e4c1d5359810d36db726d53f667ff1334a56d8
Shaurya Puri <purisha@shauryas-mbp.mynet>
2021-01-20 11:52:46 +0000

    Project setup

---
+++ navigation/tabs.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import {
+    View,
+    Image,
+    Touchable
+} from 'react-native';
+import { createBottomTabNavigator, BottomTabBar } from '@react-navigation/bottom-tabs';
+
+import { Home } from '../screens';
+
+import { COLORS, icons } from '../constants'
+
+const Tab = createBottomTabNavigator();
+
+const Tabs = () => {
+    return (
+        <Tab.Navigator
+            tabBarOptions={{
+                showLabel: false
+            }}
+        >
+            <Tab.Screen
+                name="Home"
+                component={Home}
+                options={{
+                    tabBarIcon: ({focused}) => (
+                        <Image
+                            source={icons.cutlery}
+                            resizeMeode="contain"
+                            style={{
+                                width: 25,
+                                height: 25,
+                                tintColor: focused ? COLORS.primary : COLORS.secondary
+                            }}
+                        />
+                    )
+                }}
+            />
+            <Tab.Screen
+                name="Like"
+                component={Home}
+                options={{
+                    tabBarIcon: ({focused}) => (
+                        <Image
+                            source={icons.like}
+                            resizeMeode="contain"
+                            style={{
+                                width: 25,
+                                height: 25,
+                                tintColor: focused ? COLORS.primary : COLORS.secondary
+                            }}
+                        />
+                    )
+                }}
+            />
+            <Tab.Screen
+                name="User"
+                component={Home}
+                options={{
+                    tabBarIcon: ({focused}) => (
+                        <Image
+                            source={icons.user}
+                            resizeMeode="contain"
+                            style={{
+                                width: 25,
+                                height: 25,
+                                tintColor: focused ? COLORS.primary : COLORS.secondary
+                            }}
+                        />
+                    )
+                }}
+            />
+        </Tab.Navigator>
+    )
+}
+
+export default Tabs;
